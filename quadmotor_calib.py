833f2dbdadde680d11d00399d2e7d7497f07b7bb
김연주 (yeonjoo kim) <65381985+yeunjoo121@users.noreply.github.com>
2022-03-21 18:18:13 +0900

    Add files via upload

--- quadmotor_calib.py
+++ quadmotor_calib.py
@@ -1,20 +1,27 @@
-from subprocess import call
-call("sudo pigpiod", shell=True)
 import RPi.GPIO as GPIO
 import pigpio
 import time
 
-GPIO.setmode(GPIO.BOARD)
+#GPIO.setmode(GPIO.BOARD)
 pi = pigpio.pi()
 
-motorA_pin = 22;
-motorB_pin = 27;
-motorC_pin = 4;
+motorA_pin = 27;#GPIO27 - pin13
+motorB_pin = 22;#GPIO22 - pin15
+#motorC_pin = 17;#GPIO4 - pin7
 
 ESC_PWM_MIN = 1050
 ESC_PWM_MAX = 1350
 
-for i in range(10):
-    pi.set_servo_pulsewidth(motorA_pin, i * 30 + ESC_PWM_MIN)
+print("connect ok")
 
-pi.set_servo_pulsewidth(motorA_pin, ESC_PWM_MAX)
+for i in range(10):
+    print(ESC_PWM_MIN + i * 30)
+    pi.set_servo_pulsewidth(motorA_pin, i * 30 + ESC_PWM_MIN)
+    pi.set_servo_pulsewidth(motorB_pin, i * 30 + ESC_PWM_MIN)
+    #pi.set_servo_pulsewidth(motorC_pin, i * 30 + ESC_PWM_MIN)
+    time.sleep(1)
+
+#stop
+pi.set_servo_pulsewidth(motorA_pin, ESC_PWM_MIN)
+pi.set_servo_pulsewidth(motorB_pin, ESC_PWM_MIN)
+#pi.set_servo_pulsewidth(motorC_pin, ESC_PWM_MIN)
