30eab4eae17a9bb5a6906af84b900b0d0c4bbb3a
김연주 (yeonjoo kim) <65381985+yeunjoo121@users.noreply.github.com>
2022-07-15 14:58:38 +0900

    Add files via upload

--- hils/hils_desktop_v2.py
+++ hils/hils_desktop_v2.py
@@ -1,5 +1,7 @@
+from cmath import nan
 import time
 import serial
+import random
 
 port5 = 'COM5' # port for receive * and send sensor data
 port6 = 'COM6' # port for receive rpm or angle from raspberry pi
@@ -25,44 +27,58 @@ ser6 = serial.Serial(
     )
 
 def main():
-    rpm1 = 0 #1
-    rpm2 = 0 #2
-    angle1 = 0 #3
-    angle2 = 0 #4
+    data = [float(0), float(0), float(0), float(0)]
     index = 0
+    first = 0
+    maxlen = 80
+    length = 0
     print('desktop ready')
+    
     while (True):
+        index = 0
         ch5 = ser5.read().decode()
         if (ch5 == '*'): # if read char is *, send sensor data
             # send data
-            strcmd = '*0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\r\n'
-            print('send data = ' + strcmd)
+            element = float(random.random())
+            if (first == 0):
+                first = 1
+                strcmd = '*' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + (str)(element) + '\r\n'
+            else:
+                strcmd = '*' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + ',' + (str)(element) + '\r\n'
+            print('send data = ' , strcmd)
             ser5.write(strcmd.encode())
-            for i in range(4):
-                # receive rpm or angle
-                recv = ''
-                ch = ser6.read().decode()
-                if (ch == '['): # rpm, angle format => [1]40\n or [3]3\n
-                    index = ser6.read().decode()
-                    ch = ser6.read().decode()
-                    if (ch == ']'):
-                        ch = ser6.read().decode()
+            # receive rpm or angle
+            ch = ser6.read().decode()
+            recv = ''
+            while (ch != '\n'): # 여기서 receive 예외처리 추가
+                if (ch == ','):
+                    if (index >= 4): # index가 4보다 크면 에러처리.
+                        recv = ''
                         while (ch != '\n'):
-                            recv += ch
                             ch = ser6.read().decode()
-                print('recv = ' + recv)
-                # 이제 rpm, angle값을 주어진 변수에 대입
-                if (index == '1'):
-                    rpm1 = recv
-                    print('rpm1 : ' , rpm1)
-                elif (index == '2'):
-                    rpm2 = recv
-                    print('rpm2: ' , rpm2)
-                elif (index == '3'):
-                    angle1 = recv
-                    print('angle1: ' , angle1)
-                elif (index == '4'):
-                    angle2 = recv
-                    print('angle2 : ' , angle2)        
+                        continue
+                    data[index] = float(recv)
+                    index += 1
+                    recv = ''
+                else:
+                    recv += ch
+                ch = ser6.read().decode()
+                length += 1
+                if (length > maxlen):
+                    # 오류 시 nan으로 처리. 오류가 나도 new line이 올 때까지 한 세트를 읽어들여 다음 세트에 지장이 가지 않도록 함
+                    length = 0
+                    recv = nan
+                    while (ch != '\n'):
+                        ch = ser6.read().decode()
+            data[index] = float(recv)
+            print('rpm1 : ' , data[0])
+            print('rpm2: ' , data[1])
+            print('angle1: ' , data[2])
+            print('angle2 : ' , data[3])
+            
+            print(type(data[0]))
+            print(type(data[1]))
+            print(type(data[2]))
+            print(type(data[3]))
 
 main()
